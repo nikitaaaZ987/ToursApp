048c0bf190abe16156f490cbf303866ed852e1ef
nikitaaaZ987 <nikitaslenkevic@gmail.com>
2024-12-10 18:02:39 +0800

    Add project files.

---
+++ ToursApp/HotelsPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToursApp
+{
+    /// <summary>
+    /// Interaction logic for HotelsPage.xaml
+    /// </summary>
+    public partial class HotelsPage : Page
+    {
+        public HotelsPage()
+        {
+            InitializeComponent();
+            //DGridHotels.ItemsSource = ToursikEntities.GetContext().Hotel.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Hotel));
+        }
+
+        private void BthAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void BthDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var hotelsForRemoving = DGridHotels.SelectedItems.Cast<Hotel>().ToList();
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {hotelsForRemoving.Count()} элементы?", "Внимение ",
+               MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    ToursikEntities.GetContext().Hotel.RemoveRange(hotelsForRemoving);
+                    ToursikEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены !");
+                    DGridHotels.ItemsSource = ToursikEntities.GetContext().Hotel.ToList();
+                
+                }
+
+                catch (Exception ex) 
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+
+            }
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                ToursikEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridHotels.ItemsSource = ToursikEntities.GetContext().Hotel.ToList();
+            }
+        }
+    }
+}
---
+++ ToursApp/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ToursApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new HotelsPage());
+            Manager.MainFrame = MainFrame;
+
+            ImportTours();
+        }
+
+        private void ImportTours()
+        {
+            string filePath = @"C:\Users\Nikita\Desktop\Туры2.txt";
+            string imagesPath = @"C:\Users\Nikita\Desktop\import до\Туры фото";
+
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show($"Файл {filePath} не найден.");
+                return;
+            }
+
+            if (!Directory.Exists(imagesPath))
+            {
+                MessageBox.Show($"Директория {imagesPath} не найдена.");
+                return;
+            }
+
+            var fileData = File.ReadAllLines(filePath);
+            var images = Directory.GetFiles(imagesPath);
+
+            foreach (var line in fileData)
+            {
+                var data = line.Split('\t');
+
+                if (data.Length < 6)
+                {
+                    MessageBox.Show($"Неверный формат строки: {line}");
+                    continue;
+                }
+
+                var tempTour = new Tour
+                {
+                    Name = data[0].Replace("\"", ""),
+                    TicketCount = int.Parse(data[2]),
+                    Price = decimal.Parse(data[3]),
+                    isActual = (data[4] == "0") ? false : true
+                };
+
+                foreach (var tourType in data[5].Replace("\"", "").Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    var currentType = ToursikEntities.GetContext().Type.ToList().FirstOrDefault(p => p.Name == tourType);
+                    if (currentType != null)
+                        tempTour.Type.Add(currentType);
+                }
+
+                try
+                {
+                    var imagePath = images.FirstOrDefault(p => p.Contains(tempTour.Name));
+                    if (imagePath != null)
+                    {
+                        tempTour.ImagePreview = File.ReadAllBytes(imagePath);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при чтении изображения для тура {tempTour.Name}: {ex.Message}");
+                }
+
+                ToursikEntities.GetContext().Tour.Add(tempTour);
+                ToursikEntities.GetContext().SaveChanges();
+            }
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                MainFrame.GoBack();
+            }
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                BtnBack.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                BtnBack.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
